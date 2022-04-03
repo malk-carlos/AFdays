3854f9908c07b9cc1c18cabb78067fd949ad96d4
malk-carlos <81245539+malk-carlos@users.noreply.github.com>
2022-04-03 23:18:43 +0900

    update
    日付を変更して遊べるようにしました

--- index.js
+++ index.js
@@ -26,3 +26,37 @@ function af(){
 }
 
 af()
+
+function af_change(){
+    var inp = document.getElementById("ind").value
+    var today = new Date(inp)
+    console.log(inp)
+
+    if(inp.match(/[0-9]+.[0-9]+.[0-9]+/)){
+        document.getElementById("error").style.display = "none";
+
+        var todays = today.toString().split(" ")
+        var year = today.getFullYear();
+        var month = today.getMonth() + 1;
+        var date =  today.getDate();
+        var data1 = `${todays[0]}, ${todays[1]} ${date.toString().padStart(2, "0")}, ${year}`;
+
+        if(month > 3){
+            var afday = new Date(year,3,1,0,0,0,0);
+            var ms = today.getTime() - afday.getTime();
+            var data2 = Math.floor(ms / (1000*24*60*60)) + 1;
+        } else {
+            var afday = new Date((year-1),3,1,0,0,0,0);
+            var ms = today.getTime() - afday.getTime();
+            var data2 = Math.floor(ms / (1000*24*60*60)) + 1;
+        }
+
+        var log = document.getElementById("today");
+        log.innerHTML = data1;
+
+        var AFslog = document.getElementById("AFx");
+        AFslog.innerHTML = data2;
+    } else {
+        document.getElementById("error").style.display = "unset";
+    }
+}
